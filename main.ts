8324b79981531f8d62d921ecca24d8bc958b6241
tambaboe <50433338+tambaboe@users.noreply.github.com>
2019-05-10 10:04:56 +0900

    otameshi7

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
-//% weight=70 icon="\uf075" color=#555555 block="コメント"
-namespace comment {
-    //% blockId=show_strings block="コメント %v"
-    export function noaction(text: string): void {
-    }
-}
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `);
